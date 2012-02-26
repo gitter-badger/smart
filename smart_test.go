67d95102c765355d59f368de77d73b459a50e28e
Duzy Chan <code@duzy.info>
2012-02-26 22:27:54 +0800

    test for traverse

--- smart_test.go
+++ smart_test.go
@@ -2,9 +2,18 @@ package smart
 
 import (
         //. "smart"
+        "os"
         "testing"
 )
 
 func TestTraverse(t *testing.T) {
-        //t.Error("test needed")
+        m := map[string]bool{}
+        err := traverse("main", func(fn string, fi os.FileInfo) bool {
+                if fi.Name() == "main.go" {
+                        m[fi.Name()] = true
+                }
+                return true
+        })
+        if err != nil { t.Errorf("error: %v", err) }
+        if !m["main.go"] { t.Error("main.go not found") }
 }
