6042a3c33d21751b1e7cbd4ed6f37d1a3e06cb54
Duzy Chan <code@duzy.info>
2016-04-06 16:51:44 +0800

    working around tools/shell

--- tools/shell/shell_test.go
+++ tools/shell/shell_test.go
@@ -13,17 +13,13 @@ import (
 func testCleanFiles(t *testing.T) {
         if e := os.RemoveAll("foo"); e != nil { t.Errorf("failed remove `foo'") }
         if e := os.RemoveAll("foobar"); e != nil { t.Errorf("failed remove `foobar'") }
-        if e := os.RemoveAll("o/o/o/foo"); e != nil { t.Errorf("failed remove `o/o/o/foo'") }
+        if e := os.RemoveAll("o/o/o/fooo"); e != nil { t.Errorf("failed remove `o/o/o/fooo'") }
 }
 
 func testToolsetShell(t *testing.T) {
         testCleanFiles(t)
 
-        vars := map[string]string{
-                // ...
-        }
-        
-        ctx := Build(vars)
+        ctx := Build(make(map[string]string))
         modules := ctx.GetModules()
 
         var (
@@ -34,13 +30,13 @@ func testToolsetShell(t *testing.T) {
         if m.GetName(ctx) != "touch-foo" { t.Errorf("expecting touch-foo but %v", m.GetName(ctx)); return }
         if fi, e := os.Stat("foo"); fi == nil || e != nil { t.Errorf("failed: %v", e); return }
 
-        if m, ok = modules["touch-o-o-o-foo"]; !ok { t.Errorf("expecting module touch-o-o-o-foo"); return }
-        if m.GetName(ctx) != "touch-o-o-o-foo" { t.Errorf("expecting touch-o-o-o-foo but %v", m.GetName(ctx)); return }
-        if fi, e := os.Stat("o/o/o/foo"); fi == nil || e != nil { t.Errorf("failed: %v", e); return }
-
         if m, ok = modules["touch-foobar"]; !ok { t.Errorf("expecting module touch-foobar"); return }
         if m.GetName(ctx) != "touch-foobar" { t.Errorf("expecting touch-foobar, but %v", m.GetName(ctx)); return }
         if fi, e := os.Stat("foobar"); fi == nil || e != nil { t.Errorf("failed: %v", e); return }
+
+        if m, ok = modules["touch-o-o-o-foo"]; !ok { t.Errorf("expecting module touch-o-o-o-foo"); return }
+        if m.GetName(ctx) != "touch-o-o-o-foo" { t.Errorf("expecting touch-o-o-o-foo but %v", m.GetName(ctx)); return }
+        if fi, e := os.Stat("o/o/o/fooo"); fi == nil || e != nil { t.Errorf("failed: %v", e); return }
 
         testCleanFiles(t)
 }
