fc37932a07b190abf22a720e84c0359641e54cf2
Duzy Chan <code@duzy.info>
2016-04-15 22:55:38 +0800

    working on tools/gcc

--- tools/gcc/gcc.go
+++ tools/gcc/gcc.go
@@ -4,269 +4,39 @@
 package smart
 
 import (
-        "fmt"
-        "os"
-        "strings"
-        "path/filepath"
+        //"path/filepath"
         . "github.com/duzy/smart/build"
 )
 
 func init() {
-        RegisterToolset("gcc", &toolset{})
+        e := AppendInit(HooksMap{
+                "shell": HookTable{
+                        "exec": hookExec,
+                },
+        }, `# Execute Shell Command
+template gcc
+start:!: $(me.depends) $(me.using)
+	@$(me.command) $(me.args)
+commit
+`)
+        if e != nil {
+                panic(e)
+        }
 }
 
-var (
-        patterns = []*FileMatchRule{
-                { "asm",    ^os.ModeType, `\.(s|S)$` },
-                { "c",      ^os.ModeType, `\.(c)$` },
-                { "c++",    ^os.ModeType, `\.(cpp|cxx|cc|CC|C)$` },
-                { "header", ^os.ModeType, `\.(h)$` },
-        }
-)
-
-func splitFieldsWithPrefix(ss, prefix string) (l []string) {
-        for _, s := range strings.Fields(ss) /*strings.Split(ss, " ")*/ {
-                if dont := strings.HasPrefix(s, prefix); s != "" {
-                        if prefix == "-l" {
-                                dont = dont || strings.ContainsAny(s, "/\\")
-                                dont = dont || strings.HasSuffix(s, ".so")
-                                dont = dont || strings.HasSuffix(s, ".a")
-                        }
-                        
-                        if !dont { s = prefix + s }
-
-                        l = append(l, s)
+func hookExec(ctx *Context, args Items) (res Items) {
+        /*
+        cmd := exec.Command("sh", "-c", args.Expand(ctx))
+        if cmd != nil {
+                stdout, stderr := new(bytes.Buffer), new(bytes.Buffer)
+                cmd.Stdout, cmd.Stderr = stdout, stderr
+                if err := cmd.Run(); err != nil {
+                        // TODO: report errors
+                        return
                 }
-        }
+                res = append(res, StringItem(stdout.String()))
+        } else {
+                // TODO: report errors
+        } */
         return
-}
-
-type imported struct {
-        includes, libdirs, libs []string
-        prerequisites []*Action
-        numAsm, numC, numCxx int
-}
-
-func (im *imported) load(ctx *Context) {
-        im.includes = append(im.includes, splitFieldsWithPrefix(ctx.Call("me.includes").Expand(ctx), "-I")...)
-        im.libdirs = append(im.libdirs, splitFieldsWithPrefix(ctx.Call("me.libdirs").Expand(ctx), "-L")...)
-        im.libs = append(im.libs, splitFieldsWithPrefix(ctx.Call("me.libs").Expand(ctx), "-l")...)
-}
-
-func (im *imported) importUsedModule(ctx *Context, m *Module) {
-        importVars := func() {
-                im.includes = append(im.includes, splitFieldsWithPrefix(ctx.Call("me.export.includes").Expand(ctx), "-I")...)
-                im.libdirs = append(im.libdirs, splitFieldsWithPrefix(ctx.Call("me.export.libdirs").Expand(ctx), "-L")...)
-                im.libs = append(im.libs, splitFieldsWithPrefix(ctx.Call("me.export.libs").Expand(ctx), "-l")...)
-        }
-        for _, u := range m.Using {
-                ctx.With(u, importVars)
-                im.importUsedModule(ctx, u)
-        }
-        return
-}
-
-func createCompileActions(includes, sources []string) (actions []*Action, numAsm, numC, numCxx int) {
-        as, gcc, gxx := NewExcmd("as"), NewExcmd("gcc"), NewExcmd("g++")
-        actions = CreateSourceTransformActions(sources, func(src string) (target string, c Command) {
-                var fr *FileMatchRule
-                if fi, err := os.Stat(src); err != nil {
-                        fr = MatchFileName(src, patterns)
-                } else {
-                        fr = MatchFileInfo(fi, patterns)
-                }
-
-                if fr == nil {
-                        Fatal("unknown source `%v'", src)
-                }
-
-                target = src + ".o"
-
-                switch fr.Name {
-                case "asm": c = &Compile{ GccCmd{ ex:as },  includes }; numAsm++
-                case "c":   c = &Compile{ GccCmd{ ex:gcc }, includes }; numC++
-                case "c++": c = &Compile{ GccCmd{ ex:gxx }, includes }; numCxx++
-                default: Fatal("unknown source language `%v'", src)
-                }
-                return
-        })
-        return
-}
-
-type toolset struct { BasicToolset }
-
-func (gcc *toolset) ConfigModule(ctx *Context, args Items, vars map[string]string) {
-        var (
-                kind string
-        )
-        if 0 < len(args) {
-                kind = strings.TrimSpace(args[0].Expand(ctx))
-        }
-        ctx.Set("me.kind", StringItem(kind)) // ctx.m.Set("kind", kind)
-}
-
-func (gcc *toolset) createLinkAction(ctx *Context, out, ext string, im *imported) *Link {
-        var (
-                cmd = new(Link)
-                m = ctx.CurrentModule()
-                targetName = m.GetName(ctx) + ext
-        )
-
-        cmd.out, cmd.libdirs, cmd.libs = out, im.libdirs, im.libs
-
-        m.Action = NewAction(filepath.Join(cmd.out, targetName), cmd)
-        m.Action.Prerequisites = append(m.Action.Prerequisites, im.prerequisites...)
-
-        switch {
-        case 0 == im.numCxx && 0 == im.numC && 0 < im.numAsm:
-                cmd.ex = NewExcmd("ld")
-        case 0 == im.numCxx && 0 < im.numC:
-                cmd.ex = NewExcmd("gcc")
-        case 0 <  im.numCxx:
-                cmd.ex = NewExcmd("g++")
-        }
-
-        if cmd.ex == nil {
-                Fatal("no command (%v)", m.GetName(ctx))
-        }
-
-        return cmd
-}
-
-func (gcc *toolset) createExe(ctx *Context, out string, im *imported) {
-        cmd := gcc.createLinkAction(ctx, out, "", im)
-        cmd.shared = false
-}
-
-func (gcc *toolset) createShared(ctx *Context, out string, im *imported) {
-        cmd := gcc.createLinkAction(ctx, out, ".so", im)
-        cmd.shared = true
-}
-
-func (gcc *toolset) createStatic(ctx *Context, out string, im *imported) {
-        var (
-                cmd = new(Archive)
-                m = ctx.CurrentModule()
-                targetName = m.GetName(ctx)
-        )
-
-        cmd.out = out
-
-        if !strings.HasPrefix(targetName, "lib") { targetName = "lib" + targetName }
-        if !strings.HasPrefix(targetName, ".a") { targetName = targetName + ".a" }
-
-        m.Action = NewAction(filepath.Join(cmd.out, targetName), cmd)
-        m.Action.Prerequisites = append(m.Action.Prerequisites, im.prerequisites...)
-
-        cmd.ex = NewExcmd("ar")
-}
-
-func (gcc *toolset) CreateActions(ctx *Context) bool {
-        var (
-                m = ctx.CurrentModule()
-                out = filepath.Join("out", m.GetName(ctx))
-        )
-
-        // Add proper prefixes to includes, libdirs, libs.
-        im := new(imported)
-        im.load(ctx)
-
-        // Import includes and libs from using modules.
-        if 0 < len(m.Using) {
-                im.importUsedModule(ctx, m)
-        }
-
-        sources := m.GetSources(ctx)
-        if len(sources) == 0 { Fatal("no sources (%v)", m.GetName(ctx)) }
-
-        im.prerequisites, im.numAsm, im.numC, im.numCxx = createCompileActions(im.includes, sources)
-
-        switch m.Get(ctx, "kind") {
-        case "exe":    gcc.createExe(ctx, out, im)
-        case "shared": gcc.createShared(ctx, out, im)
-        case "static": gcc.createStatic(ctx, out, im)
-        default: Fatal(fmt.Sprintf("unknown type `%v'", m.Get(ctx, "kind")))
-        }
-
-        return m.Action != nil
-}
-
-type GccCmd struct {
-        ex *Excmd
-        out string
-}
-
-type Compile struct {
-        GccCmd
-        includes []string
-}
-
-func (c *Compile) Execute(targets []string, prerequisites []string) bool {
-        if numExpected := len(targets); numExpected == 0 {
-                Fatal("linking for zero")
-        } else if 1 < numExpected {
-                Fatal("linking multiple targets")
-        }
-
-        var (
-                target = targets[0]
-                args = []string{ "-o", target, "-c" }
-        )
-
-        if c.ex == nil { Fatal("nil command (%v)", target) }
-
-        args = append(args, c.includes...)
-        args = append(args, prerequisites...)
-        return c.ex.Run(target, args...)
-}
-
-type Link struct {
-        GccCmd
-        shared bool
-        libdirs, libs []string
-}
-
-func (c *Link) Execute(targets []string, prerequisites []string) bool {
-        if numExpected := len(targets); numExpected == 0 {
-                Fatal("linking for zero")
-        } else if 1 < numExpected {
-                Fatal("linking multiple targets")
-        }
-
-        var (
-                target = targets[0]
-                args = []string{ "-o", target }
-        )
-
-        if c.ex == nil { Fatal("nil command (%v)", target) }
-        if c.shared { args = append(args, "-shared") }
-
-        args = append(args, prerequisites...)
-        args = append(args, c.libdirs...)
-        args = append(args, c.libs...)
-
-        c.ex.SetMkdir(c.out)
-        return c.ex.Run(target, args...)
-}
-
-type Archive struct {
-        GccCmd
-}
-
-func (c *Archive) Execute(targets []string, prerequisites []string) bool {
-       if numExpected := len(targets); numExpected == 0 {
-                Fatal("linking for zero")
-        } else if 1 < numExpected {
-                Fatal("linking multiple targets")
-        }
-
-        var (
-                target = targets[0]
-                args = []string{ "crs", target }
-        )
-
-        args = append(args, prerequisites...)
-
-        c.ex.SetMkdir(c.out)
-        return c.ex.Run(target, args...)
 }
--- tools/gcc/gcc_test.go
+++ tools/gcc/gcc_test.go
@@ -4,7 +4,6 @@
 package smart
 
 import (
-        "path/filepath"
         "testing"
         "fmt"
         "os"
@@ -27,7 +26,7 @@ func testCleanFiles(t *testing.T) {
 func testToolsetGcc(t *testing.T) {
         testCleanFiles(t)
 
-        ctx := Build(ComputeTestRunParams())
+        ctx := Build(make(map[string]string))
         modules := ctx.GetModules()
 
         var m *Module
@@ -36,64 +35,26 @@ func testToolsetGcc(t *testing.T) {
         if m.GetName(ctx) != "foo_gcc_exe" { t.Errorf("expecting module foo_gcc_exe, but %v", m.GetName(ctx)); return }
         if m.GetDir(ctx) != "exe" { t.Errorf("expecting dir `exe', but %v", m.GetDir(ctx)); return }
         if m.Get(ctx, "kind") != "exe" { t.Errorf("expecting exe for foo_gcc_exe, but %v", m.Get(ctx, "kind")); return }
-        if m.Action == nil { t.Errorf("no action for the module"); return }
-        if l := len(m.Action.Targets); l != 1 { t.Errorf("expection 1 targets, but %v", l); return }
-        if fn := filepath.Join("out", m.GetName(ctx), m.GetName(ctx)); m.Action.Targets[0] != fn { t.Errorf("expecting action target %v, but %v", fn, m.Action.Targets); return }
-        if l := len(m.Action.Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if l := len(m.Action.Prerequisites[0].Targets); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Targets[0]; s != filepath.Join("exe", "foo.c.o") { t.Errorf("expect exe/foo.c.o, but %v (%v)", s, m.Action.Prerequisites[0].Targets); return }
-        if l := len(m.Action.Prerequisites[0].Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Prerequisites[0].Targets[0]; s != filepath.Join("exe", "foo.c") { t.Errorf("expect exe/foo.c, but %v (%v)", s, m.Action.Prerequisites[0].Prerequisites[0].Targets); return }
 
         if m, ok = modules["foo_shared"]; !ok { t.Errorf("expecting module foo_shared"); return }
         if m.GetName(ctx) != "foo_shared" { t.Errorf("expecting module foo_shared, but %v", m.GetName(ctx)); return }
         if m.GetDir(ctx) != "shared" { t.Errorf("expecting dir `shared', but %v", m.GetDir(ctx)); return }
         if m.Get(ctx, "kind") != "shared" { t.Errorf("expecting shared for foo_shared, but %v", m.Get(ctx, "kind")); return }
-        if m.Action == nil { t.Errorf("no action for the module"); return }
-        if l := len(m.Action.Targets); l != 1 { t.Errorf("expection 1 targets, but %v", l); return }
-        if l := len(m.Action.Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if l := len(m.Action.Prerequisites[0].Targets); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Targets[0]; s != filepath.Join("shared", "foo.c.o") { t.Errorf("expect shared/foo.c.o, but %v (%v)", s, m.Action.Prerequisites[0].Targets); return }
-        if l := len(m.Action.Prerequisites[0].Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Prerequisites[0].Targets[0]; s != filepath.Join("shared", "foo.c") { t.Errorf("expect shared/foo.c, but %v (%v)", s, m.Action.Prerequisites[0].Prerequisites[0].Targets); return }
 
         if m, ok = modules["foo_static"]; !ok { t.Errorf("expecting module foo_static"); return }
         if m.GetName(ctx) != "foo_static" { t.Errorf("expecting module foo_static, but %v", m.GetName(ctx)); return }
         if m.GetDir(ctx) != "static" { t.Errorf("expecting dir `static', but %v", m.GetDir(ctx)); return }
         if m.Get(ctx, "kind") != "static" { t.Errorf("expecting static for foo_static, but %v", m.Get(ctx, "kind")); return }
-        if m.Action == nil { t.Errorf("no action for the module"); return }
-        if l := len(m.Action.Targets); l != 1 { t.Errorf("expection 1 targets, but %v", l); return }
-        if l := len(m.Action.Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if l := len(m.Action.Prerequisites[0].Targets); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Targets[0]; s != filepath.Join("static", "foo.c.o") { t.Errorf("expect static/foo.c.o, but %v (%v, %v)", s, m.GetName(ctx), m.Action.Prerequisites[0].Targets); return }
-        if l := len(m.Action.Prerequisites[0].Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Prerequisites[0].Targets[0]; s != filepath.Join("static", "foo.c") { t.Errorf("expect static/foo.c, but %v (%v)", s, m.Action.Prerequisites[0].Prerequisites[0].Targets); return }
 
         if m, ok = modules["foo_gcc_exe_use_shared"]; !ok { t.Errorf("expecting module foo_gcc_exe_use_shared"); return }
         if m.GetName(ctx) != "foo_gcc_exe_use_shared" { t.Errorf("expecting module foo_gcc_exe_use_shared, but %v", m.GetName(ctx)); return }
         if m.GetDir(ctx) != "exe_use_shared" { t.Errorf("expecting dir `exe_use_shared', but %v", m.GetDir(ctx)); return }
         if m.Get(ctx, "kind") != "exe" { t.Errorf("expecting exe for foo_gcc_exe, but %v", m.Get(ctx, "kind")); return }
-        if m.Action == nil { t.Errorf("no action for the module"); return }
-        if l := len(m.Action.Targets); l != 1 { t.Errorf("expection 1 targets, but %v", l); return }
-        if fn := filepath.Join("out", m.GetName(ctx), m.GetName(ctx)); m.Action.Targets[0] != fn { t.Errorf("expecting action target %v, but %v", fn, m.Action.Targets); return }
-        if l := len(m.Action.Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if l := len(m.Action.Prerequisites[0].Targets); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Targets[0]; s != filepath.Join("exe_use_shared", "foo.c.o") { t.Errorf("expect exe_use_shared/foo.c.o, but %v (%v)", s, m.Action.Prerequisites[0].Targets); return }
-        if l := len(m.Action.Prerequisites[0].Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Prerequisites[0].Targets[0]; s != filepath.Join("exe_use_shared", "foo.c") { t.Errorf("expect exe_use_shared/foo.c, but %v (%v)", s, m.Action.Prerequisites[0].Prerequisites[0].Targets); return }
 
         if m, ok = modules["foo_gcc_exe_use_static"]; !ok { t.Errorf("expecting module foo_gcc_exe_static"); return }
         if m.GetName(ctx) != "foo_gcc_exe_use_static" { t.Errorf("expecting module foo_gcc_exe_static, but %v", m.GetName(ctx)); return }
         if m.GetDir(ctx) != "exe_use_static" { t.Errorf("expecting dir `exe_use_static', but %v", m.GetDir(ctx)); return }
         if m.Get(ctx, "kind") != "exe" { t.Errorf("expecting exe for foo_gcc_exe_use_static, but %v", m.Get(ctx, "kind")); return }
-        if m.Action == nil { t.Errorf("no action for the module"); return }
-        if l := len(m.Action.Targets); l != 1 { t.Errorf("expection 1 targets, but %v", l); return }
-        if fn := filepath.Join("out", m.GetName(ctx), m.GetName(ctx)); m.Action.Targets[0] != fn { t.Errorf("expecting action target %v, but %v", fn, m.Action.Targets); return }
-        if l := len(m.Action.Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if l := len(m.Action.Prerequisites[0].Targets); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Targets[0]; s != filepath.Join("exe_use_static", "foo.c.o") { t.Errorf("expect exe_use_static/foo.c.o, but %v (%v)", s, m.Action.Prerequisites[0].Targets); return }
-        if l := len(m.Action.Prerequisites[0].Prerequisites); l != 1 { t.Errorf("expecting 1 prequisite, but %v", l); return }
-        if s := m.Action.Prerequisites[0].Prerequisites[0].Targets[0]; s != filepath.Join("exe_use_static", "foo.c") { t.Errorf("expect exe_use_static/foo.c, but %v (%v)", s, m.Action.Prerequisites[0].Prerequisites[0].Targets); return }
 
         if fi, e := os.Stat("out"); fi == nil || e != nil || !fi.IsDir() { t.Errorf("failed: %v", e); return }
         if fi, e := os.Stat("out/foo_shared"); fi == nil || e != nil || !fi.IsDir() { t.Errorf("failed: %v", e); return }
